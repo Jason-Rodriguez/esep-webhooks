fc4f4a9212096d8fc2b54a18ecd7a99b99603369
Jason-Rodriguez <73567518+Jason-Rodriguez@users.noreply.github.com>
2023-03-31 00:02:26 -0400

    Added Newtonsoft

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,9 +16,10 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context) {
+    
+    public string FunctionHandler(object input, ILambdaContext context) {
 
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input);
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
 
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         
